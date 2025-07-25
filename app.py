d449bf488fa94389c99f7c7587e7ebba82b19153
Karthi <karthi001bus@hotmail.com>
2025-07-25 21:44:37 +0530

    Upload images

---
+++ app.py
@@ -0,0 +1,68 @@
+import os
+import pyodbc
+from flask import (Flask, redirect, render_template, request,send_from_directory, url_for,jsonify,session,send_file)
+from dotenv import load_dotenv
+from io import BytesIO
+
+app = Flask(__name__)
+
+
+load_dotenv()
+
+server = os.getenv("SQL_SERVER")
+database = os.getenv("SQL_DATABASE")
+username = os.getenv("SQL_USERNAME")
+password = os.getenv("SQL_PASSWORD")
+driver = '{ODBC Driver 18 for SQL Server}'
+
+connection_string = f'Driver={driver};Server={server};Database={database};Uid={username};Pwd={password};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+
+
+conn = pyodbc.connect(connection_string)
+
+@app.route('/')
+def index():
+
+    conn=pyodbc.connect(connection_string)
+    cursor=conn.cursor()
+    query="Select ImgID,Imagedata,Imagetype,Name from [Cloths.Data]"
+    cursor.execute(query)
+    Images=cursor.fetchall()
+
+    return render_template('index.html',Images=Images)
+
+@app.route('/admin')
+def admin():
+    conn=pyodbc.connect(connection_string)
+    cursor=conn.cursor()
+    query="Select ImgID,Imagedata,Imagetype,Name from [Cloths.Data]"
+    cursor.execute(query)
+    Images=cursor.fetchall()
+    return render_template('admin/dashboard.html',Images=Images)
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file=request.files['image']
+    if file:
+        image_data=file.read()
+        conn=pyodbc.connect(connection_string)
+        cursor=conn.cursor()
+        query="Insert into [Cloths.Data](Name,ClothType,Imagetype,Imagedata) Values (?,?,?,?)"
+        cursor.execute(query,(file.filename,file.filename,file.content_type, image_data,))
+        conn.commit()
+    return redirect('admin')
+
+@app.route('/image/<int:image_id>')
+def image(image_id):
+    print("Hello")
+    conn=pyodbc.connect(connection_string)
+    cursor=conn.cursor()
+    cursor.execute("SELECT * FROM [Cloths.data] WHERE ImgId = ?", image_id)
+    row = cursor.fetchone()
+    print("Hello")
+    if row:
+        return send_file(BytesIO(row.Imagedata), mimetype=row.Imagetype)
+    return "Image not found", 404
+
+if __name__=='__main__':
+    app.run(debug=True,port=3000)
--- app/utils/security.py
+++
@@ -1,8 +0,0 @@
-# app/utils/security.py
-from werkzeug.security import generate_password_hash, check_password_hash
-
-def hash_password(password):
-    return generate_password_hash(password)
-
-def verify_password(stored_password, input_password):
-    return check_password_hash(stored_password, input_password)
